139e3562b8b9b79f50bc526df8908f90c1745e53
akhachat <akhachat@softserveinc.com>
2021-09-18 17:36:56 +0300

    first commit in my life

---
+++ Cesar.py
@@ -0,0 +1,14 @@
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+key = int(input('Key:'))
+str1 = input()
+res = ''
+
+for i in str1:
+    if i == ' ':
+        res+=' '
+        continue
+    
+    res += alpha[(alpha.find(i) + key) % len(alpha)]
+    
+print('Result: "' + res + '"')
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Encryptions
+# Just for test
